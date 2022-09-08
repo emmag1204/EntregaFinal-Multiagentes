50f3dee6e6c5fc92a0196a365135ec632abc34b6
BrandonMagana <magana.brandon12@outlook.com>
2022-09-08 09:26:10 -0500

    Agentpy Simulation and server

---
+++ App/agent.py
@@ -0,0 +1,179 @@
+import agentpy as ap
+#visualizations
+from random import randint
+import CircularDLinkedList
+import json
+
+ENTRIES_INDEXES = [3, 16, 11, 24]
+EXITS_INDEXES = [1, 12, 15, 26]
+SPAWNS = [(18,1),(33,18) ,(1, 16), (16,33)]
+CarPositions = {}
+
+class CAR(ap.Agent):
+    def setup(self):
+        pass
+
+    def agent_method(self):
+        pass
+
+class StreetModel(ap.Model):
+
+    def makeCircle(self,startX,startY,r):
+        map = []
+        Epsilon = 2.2
+        for y in range(startX-r,startX+r+1):
+            for x in range(startY-r,startY+r+1):
+                if abs((x-startX)**2 + (y-startY)**2 - r**2) < Epsilon**2:
+                    map.append((x,y))
+        return map
+
+    def setup(self):
+        # Create grid (street)
+        print("GENERE MODELO")
+        self.street = ap.Grid(self, [self.p.size]*2, track_empty=True)
+    
+        # Draw Roundabout
+        rounaboutPoints = self.makeCircle(int(self.p.size/2),int(self.p.size/2),3)
+        self.roundabout = ap.AgentList(self,len(rounaboutPoints))
+        self.street.add_agents(self.roundabout,rounaboutPoints)
+        self.roundabout.condition = 2
+
+        #Draw Route Around Roundabout
+        carRoutePoints = self.makeCircle(int(self.p.size/2),int(self.p.size/2),5)
+        self.carRoute = ap.AgentList(self,len(carRoutePoints))
+        self.street.add_agents(self.carRoute,carRoutePoints)
+        self.carRoute.condition = 0
+
+        #Modeling Route on Circular Doubly Linked List 
+        self.route = CircularDLinkedList.buildRoundAbout(carRoutePoints)
+        self.route.setNewStart(carRoutePoints[0])
+
+        #Defining Roundabout Entries and Exits based on indexes
+        self.entries = [carRoutePoints[i] for i in ENTRIES_INDEXES]
+        self.exits = [carRoutePoints[i] for i in EXITS_INDEXES]
+        
+        #Setting Up Car Spawns
+        self.spawns = ap.AgentList(self, 4)
+        self.setUpSpawns()
+        self.street.add_agents(self.spawns, [spawn.location for spawn in self.spawns])
+
+        #Preparing AgentList of Cars
+        self.spawned_cars = 0
+        nCars = self.nCars = int(self.p["ncars"])
+        self.cars = ap.AgentList(self, nCars)
+        self.paso = 0
+    
+    def setUpSpawns(self):
+        for i, spawn in enumerate(self.spawns):
+            spawn.condition = 3
+            spawn.available = 1
+            spawn.location = SPAWNS[i]
+
+    def setUpCar(self, car):
+        STARTING_MOVEMENT = [(0,1), (-1,0), (1, 0), (0, -1)]
+        ENDING_MOVEMENT = [(0,-1), (1,0), (-1, 0), (0, 1)]
+        
+        car.name = "Car"+ str(self.spawned_cars)
+        car.onRoute = -1
+        car.condition= 1
+        
+        entrie = randint(0, 3)
+        car.start = self.entries[entrie]
+        car.position = car.spawn = SPAWNS[entrie]  
+        car.initial_movement = STARTING_MOVEMENT[entrie]
+        
+        exit = randint(0, 3)
+        car.exit = self.exits[exit]
+        car.ending_movement = ENDING_MOVEMENT[exit]
+        return car
+
+    def getSpawn(self, position):
+        for index, spawn in enumerate(self.spawns):
+            if spawn.location == position:
+                return index 
+
+    def updateCarPosition(self, current_position, movement):
+            x, y = current_position
+            xi, yi = movement
+            return (x + xi, y + yi)
+
+    def step(self):
+        if self.spawned_cars < self.nCars:
+            # print(self.spawned_cars)
+            carList = ap.AgentList(self, 1, CAR)
+            carAgent = self.setUpCar(carList[0])
+            spawn_index = self.getSpawn(carAgent.spawn)
+            if self.spawns[spawn_index].available == 1:
+                print(f"placing {self.spawned_cars + 1}")
+                self.cars[self.spawned_cars] = carAgent
+                self.street._add_agent(self.cars[self.spawned_cars], carAgent.spawn, 'agents')
+                self.spawned_cars += 1                     
+                self.spawns[spawn_index].available = 0
+
+        cars = self.cars[:self.spawned_cars]
+        # print(self.paso)
+        CarPositions[str(self.paso)] = {}
+        for car in cars:
+            #SPAWNING
+            CarPositions[str(self.paso)][car.name] = car.position
+            
+            if car.onRoute == -1:
+                spawn_index = self.getSpawn(car.position)
+                car_prev = car.position
+                new_position = self.updateCarPosition(car.position, car.initial_movement)
+                car.position = new_position
+                self.street.move_by(car, car.initial_movement)
+                self.spawns[spawn_index].available = 1
+                car.onRoute = 0
+                
+            #HEADING TO ROUNDABOUT
+            if car.onRoute == 0:
+                new_position = self.updateCarPosition(car.position, car.initial_movement)
+                if new_position != car.start:
+                    self.street.move_by(car, car.initial_movement)
+                    car.position = new_position
+                elif new_position == car.start:
+                    start = self.route.findNode(new_position)
+                    if start.available == 1:
+                        self.street.move_by(car, car.initial_movement)
+                        car.position = new_position
+                        start.available = 0
+                        car.onRoute = 1
+
+            #ON ROUNDABOUT
+            if car.onRoute == 1:
+                current_position = self.route.findNode(car.position)
+                next_position = current_position.next
+                if current_position.data != car.exit and next_position.available == 1:
+                    self.street.move_to(car, next_position.data)
+                    current_position.available = 1
+                    car.position = next_position.data
+                    next_position.available = 0
+                elif current_position.data == car.exit:
+                    car.onRoute = 2
+
+            #EXITING ROUNDABOUT
+            if car.onRoute == 2:
+                if car.position == car.exit:
+                    current_position = self.route.findNode(car.position)
+                    current_position.available = 1
+                self.street.move_by(car, car.ending_movement)
+                car.position = self.updateCarPosition(car.position,car.ending_movement)
+
+        self.paso += 1
+        if self.paso == self.p["steps"]:
+            self.createJson("positions.json")    
+            
+    def createJson(self, name):
+        with open(name, 'w') as file:
+            json_string = json.dumps(CarPositions, default=lambda o: o.__dict__, sort_keys=False, indent=4)
+            file.write(json_string)
+
+def runModel(nCars):
+    parameters = {
+        'size': 35,
+        'n': 10, # Height and length of the grid
+        'steps': 100,
+        'ncars': nCars
+    }
+    return StreetModel(parameters)
--- App/app.py
+++ App/app.py
@@ -1,15 +1,26 @@
-from crypt import methods
-from flask import Flask, request
-from flask_json import FlaskJSON
+from flask import Flask
+from agent import *
+import json
+import os
+from flask import json
 
 app = Flask(__name__)
-json = FlaskJSON(app)
 
-@app.route("/agentpy_json", methods = ['POST'])
-def agentpy_send_data():
-    data = request.get_json(force=True)
-    print(data)
-    return "<p>Hello, World!</p>"
+@app.route("/simulate", methods = ['GET'])
+def agentpy_send_car_positions():
+    modelo = runModel(5)
+    modelo.run()
+
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = SITE_ROOT + "/positions.json"
+    
+    data = json.load(open(json_url))
+    response = app.response_class(
+        response=json.dumps(data),
+        mimetype='application/json'
+    )
+
+    return response
 
 if __name__ == '__main__':
     app.run()
